b10ef6dea4c22712d657c3cf5bc806baa0cc0c60
shar <faethychan@gmail.com>
2023-10-30 11:11:11 +0800

    Organized movement code and added arrow key support

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,17 +7,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]private float movementSpeed;
 
     private void Update() {
-        if (Input.GetKey(KeyCode.W)) {
+        Movement(movementSpeed);
+    }
+
+    private void Movement(float movementSpeed) {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
             transform.position += transform.up * movementSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S)) {
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
             transform.position -= transform.up * movementSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
             transform.position -= transform.right * movementSpeed * Time.deltaTime;
             transform.localScale = new Vector3(1, 1, 1);
         }
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
             transform.position += transform.right * movementSpeed * Time.deltaTime;
             transform.localScale = new Vector3(-1, 1, 1);
         }
