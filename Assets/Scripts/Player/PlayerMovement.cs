5bb2a21509f6002a5232acc4d5f820485e636857
shar <faethychan@gmail.com>
2023-10-30 11:06:18 +0800

    Added player and its movement and animations

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private float movementSpeed;
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += transform.up * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position -= transform.up * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position -= transform.right * movementSpeed * Time.deltaTime;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += transform.right * movementSpeed * Time.deltaTime;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/SpinningObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningObject : MonoBehaviour
+{
+    [Tooltip("How much the object will spin in 1 second.")]
+    [SerializeField]private float playerSpinSpeed = default;
+    
+    [Tooltip("Currently unused.")]
+    [SerializeField]private bool isClockwiseSpinDirection = default;
+
+    private void Update() {
+        SpinObject(playerSpinSpeed, isClockwiseSpinDirection);
+    }
+
+    private void SpinObject(float spinSpeed, bool isClockwise) {
+        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+    }
+}
