024c7dfae62089dc2a58708787174209d3b2ae8e
shar <faethychan@gmail.com>
2023-11-08 03:16:26 +0800

    Invincibility frames added

--- Assets/Scripts/Components/HurtboxComponent.cs
+++ Assets/Scripts/Components/HurtboxComponent.cs
@@ -7,15 +7,27 @@ public class HurtboxComponent : MonoBehaviour
     [SerializeField]private string colliderObjectTag = default;
     private HealthComponent healthComponent = default;
 
+    [Header("Invincibility Frames Parameters")]
+    [SerializeField]private float invincibilityTime = default;
+    private float timerForIFrames = default;
+
     private void Start() {
         healthComponent = GetComponent<HealthComponent>();
     }
 
+    private void Update()
+    {
+        timerForIFrames -= Time.deltaTime;
+        timerForIFrames = Mathf.Max(timerForIFrames, 0);
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == colliderObjectTag) {
+        if (other.gameObject.tag == colliderObjectTag && timerForIFrames == 0) {
             int damageValue = other.gameObject.GetComponent<BulletComponent>().damageValue;
             other.gameObject.SetActive(false);
             healthComponent.TakeDamage(damageValue);
+
+            timerForIFrames = invincibilityTime;
         }
     }
 }
