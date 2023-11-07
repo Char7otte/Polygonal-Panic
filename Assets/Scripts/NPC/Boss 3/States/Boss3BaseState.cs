097716777e55304e02031fa3a45552b57f5a43d2
shar <faethychan@gmail.com>
2023-11-07 15:25:58 +0800

    State machine code removed
    nothing works

--- Assets/Scripts/NPC/Boss 3/States/Boss3BaseState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Boss3BaseState
-{
-    public abstract void EnterState(Boss3StateManager phase);
-    public abstract void UpdateState(Boss3StateManager phase);
-}
