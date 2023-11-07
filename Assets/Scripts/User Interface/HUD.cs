a3246033637f755e3beadff20bdbde20f23b4ab1
shar <faethychan@gmail.com>
2023-11-08 03:29:46 +0800

    Player heart system added

---
+++ Assets/Scripts/User Interface/HUD.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    private GameObject player = default;
+    private HealthComponent playerHealthComponent = default;
+    private int playerHealth = default;
+
+    [Header("Heart Points")]
+    [SerializeField]private Image[] heartPoints = default;
+    [SerializeField]private Sprite fullHeart;
+    [SerializeField]private Sprite emptyHeart;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        playerHealthComponent = player.GetComponent<HealthComponent>();
+    }
+
+    private void Update()
+    {
+        playerHealth = playerHealthComponent.currentHealth;
+
+        for (int i = 0; i < heartPoints.Length; i++)
+        {
+            if (i < playerHealth)
+            {
+                heartPoints[i].sprite = fullHeart;
+            }
+            else
+            {
+                heartPoints[i].sprite = emptyHeart;
+            }
+        }
+    }
+}
