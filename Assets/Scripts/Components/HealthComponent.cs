fb1cb8af1e0fbe583869844e1db54ae6d5a4d708
shar <faethychan@gmail.com>
2023-11-15 11:16:24 +0800

    Damage strobe effect implemented

--- Assets/Scripts/Components/HealthComponent.cs
+++ Assets/Scripts/Components/HealthComponent.cs
@@ -6,16 +6,52 @@ public class HealthComponent : MonoBehaviour
 {
     public int maxHealth = default;
     public int currentHealth = default;
+    public SpriteRenderer spriteRenderer = default;
+    private bool startedCoroutine = false;
 
     private void Start() {
         currentHealth = maxHealth;
+        spriteRenderer = CheckForSpriteRenderer();
     }
 
     public void TakeDamage(int damageValue) {
         currentHealth -= damageValue;
+        DamageProbeEffect();
         
         if (currentHealth <= 0) {
             Destroy(this.gameObject);
         }
+    }
+
+    private SpriteRenderer CheckForSpriteRenderer() {
+        SpriteRenderer _spriteRenderer = this.gameObject.GetComponentInChildren<SpriteRenderer>();
+        if (_spriteRenderer != null)
+        {
+            Debug.Log("Child with SpriteRenderer found: " + _spriteRenderer.gameObject.name);
+            return _spriteRenderer;
+        }
+        else
+        {
+            Debug.Log("No child with SpriteRenderer found.");
+            return null;
+        }
+    }
+
+    private void DamageProbeEffect() {
+        if (spriteRenderer == null) return;
+        if (!startedCoroutine) StartCoroutine(DamageStrobeAnimation());
+    }
+
+    IEnumerator DamageStrobeAnimation() {
+        startedCoroutine = true;
+
+        Color currentColor = spriteRenderer.color;
+        currentColor.a = 0;
+        spriteRenderer.color = currentColor;
+
+        yield return new WaitForSeconds(0.05f);
+        currentColor.a = 255;
+        spriteRenderer.color = currentColor;
+        startedCoroutine = false;
     }
 }
