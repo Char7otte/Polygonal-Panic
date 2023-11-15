7be0d346731d1b7147dc448b2b50e254b0a26c91
shar <faethychan@gmail.com>
2023-11-16 06:02:49 +0800

    Game will be able to restart after the first time now

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -36,7 +36,6 @@ public class GameManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
